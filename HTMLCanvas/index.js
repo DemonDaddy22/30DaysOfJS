8786505156681721df86f6de8ea813ae53d93217
Rohan Gupta <guptarohan369@gmail.com>
2020-08-11 00:04:37 +0530

    add js for canvas

--- HTMLCanvas/index.js
+++ HTMLCanvas/index.js
@@ -1,5 +1,38 @@
 const canvas = document.querySelector('#canvas');
-const ctx = canvas.msGetRegionContent('2d');
+const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
-camvas.height = window.innerHeight;
+canvas.height = window.innerHeight;
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let canDraw = false;
+
+let endX = 0, endY = 0, hue = 0, width = 50, direction = true;
+
+function drawing(e) {
+    if (!canDraw) return;
+    
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.lineWidth = width;
+
+    ctx.beginPath();
+    ctx.moveTo(endX, endY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
+    [endX, endY] = [e.offsetX, e.offsetY];
+
+    hue = (hue + 1) % 360;
+    width = direction ? width + 1 : width - 1;
+    if (width > 100 || width < 50) direction = !direction;
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    canDraw = true;
+    [endX, endY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mousemove', drawing);
+canvas.addEventListener('mouseup', () => canDraw = false);
+canvas.addEventListener('mouseout', () => canDraw = false);
