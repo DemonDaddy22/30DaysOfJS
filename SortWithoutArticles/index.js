440459ef647fc4c4f9ab3f26688bc9a5873d1945
Rohan Gupta <guptarohan369@gmail.com>
2020-08-19 22:29:13 +0530

    add styling and js

---
+++ SortWithoutArticles/index.js
@@ -0,0 +1,13 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything',
+    'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function stripArticle(bandName) {
+    return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+
+bands.sort((a, b) => stripArticle(a) < stripArticle(b) ? -1 : 1);
+
+const ul = document.querySelector('.bands');
+ul.innerHTML = bands.map(band => {
+    return `<li>${band}</li>`;
+}).join('');
