f6292849e88ca6260ce985a8e01c8fbce8700543
Rohan Gupta <guptarohan369@gmail.com>
2020-08-21 22:34:50 +0530

    update DOM on input speech

--- NativeSpeechRecognition/index.js
+++ NativeSpeechRecognition/index.js
@@ -9,7 +9,18 @@ let p = document.createElement('p');
 words.appendChild(p);
 
 recognition.addEventListener('result', e => {
-    console.log(e);
+    const transcript = [...e.results]
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+
+    const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
+    p.textContent = poopScript;
+
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
 })
 
 recognition.addEventListener('end', recognition.start);
