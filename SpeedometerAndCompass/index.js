258dae5f6033e1014bb2d49f07698a99b1b8608c
Rohan Gupta <guptarohan369@gmail.com>
2020-08-23 01:20:25 +0530

    update README.md

--- SpeedometerAndCompass/index.js
+++ SpeedometerAndCompass/index.js
@@ -4,4 +4,7 @@ const speed = document.querySelector('.speed-value');
 navigator.geolocation.watchPosition(data => {
     speed.textContent = data.coords.speed;
     arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+}, error => {
+    console.error(error);
+    alert('You need to give permissions to access the APP!');
 });
