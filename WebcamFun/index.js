7bbebc0a32f73a8a6ca4fa5cf6a249ced6b2d1da
Rohan Gupta <guptarohan369@gmail.com>
2020-08-23 22:21:02 +0530

    add styling and webcam

---
+++ WebcamFun/index.js
@@ -0,0 +1,20 @@
+const video = document.querySelector('.player');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+        video.srcObject = localMediaStream;
+        video.play();
+    })
+    .catch(err => {
+        console.error(err);
+        alert('Please turn ON the camera to use the app!');
+    })
+}
+
+getVideo();
