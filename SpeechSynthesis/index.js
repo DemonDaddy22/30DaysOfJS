5fa99f3b6e488b00d47e5d0926bd53c715473d6b
Rohan Gupta <guptarohan369@gmail.com>
2020-08-25 23:03:40 +0530

    add event listeners

--- SpeechSynthesis/index.js
+++ SpeechSynthesis/index.js
@@ -11,6 +11,10 @@ speech.text = document.querySelector('[name="text"]').value;
 // console.log(speech);
 
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', handleChangeOption));
+speakButton.addEventListener('click', toggleVoice);
+stopButton.addEventListener('click', () => toggleVoice(false));
 
 function populateVoices() {
     voices = this.getVoices();
@@ -18,4 +22,20 @@ function populateVoices() {
 
     const voiceOptions = voices.map(voice => `<option value=${voice.name}>${voice.name} (${voice.lang})</option>`).join('');
     voicesDropdown.innerHTML = voiceOptions;
+}
+
+function setVoice() {
+    speech.voice = voices.find(voice => voice.name === this.value);
+    // console.log(speech);
+    toggleVoice();
+}
+
+function toggleVoice(startOver = true) {
+    speechSynthesis.cancel();
+    if (startOver) speechSynthesis.speak(speech);
+}
+
+function handleChangeOption() {
+    speech[this.name] = this.value;
+    toggleVoice();
 }
